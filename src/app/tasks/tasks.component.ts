3722ca1ceec85858c4f985c9c86da347c244b287
desktop <desktop>
2017-08-24 19:56:35 +0300

    Working on Calculating next due date for task, Need to include moment js

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { Api, ApiRoutes, Request } from './../helper/api';
 import { Task } from './task.model';
-import { TASK_FREQUENCY_TYPES, TASK_TYPES} from './constants';
+import { TASK_FREQUENCY_TYPES, TASK_TYPES } from './constants';
 
 @Component({
     templateUrl: './tasks.html'
 })
 export class TasksComponent {
     tasks = []
-    currentTask = {};
+    currentTask: Task = {};
     taskTypes = TASK_TYPES;
     frequencyTypes = TASK_FREQUENCY_TYPES;
 
@@ -18,24 +18,63 @@ export class TasksComponent {
 
     fetchTasks() {
         this.api.sendRequest({ endpoint: ApiRoutes.FETCH_ALL_TASKS, method: 'get' })
-            .subscribe(res =>{
+            .subscribe(res => {
                 let tasks = res.json();
-                (tasks as Task[]).forEach(t=>{
+                (tasks as Task[]).forEach(t => {
                     t.customer = t.customer[0];
                     t.user = t.user[0];
                 });
-                
+
                 this.tasks = tasks;
             }
-        ,
+            ,
             err => console.log(err));
     }
 
-    getFrequencyTypeText(frequencyValue){
-        return this.frequencyTypes.find((ft)=>ft.value == frequencyValue).text;
+    getFrequencyTypeText(frequencyValue) {
+        return this.frequencyTypes.find((ft) => ft.value == frequencyValue).text;
     }
 
-    setCurrentTask(task:Task){
+    getNextDueDate(task: Task) {
+        if (task.completed) {
+            return "Task Completed";
+        }
+        else {
+            let startingDate: Date;
+            let nextDueDate: string;
+            if (task.startsOn) {
+                startingDate = new Date(task.startsOn);
+            }
+
+
+            switch (task.frequency) {
+                case TASK_FREQUENCY_TYPES[0].value:
+                    nextDueDate = startingDate.toDateString();
+                    break;
+                case TASK_FREQUENCY_TYPES[1].value:
+                    nextDueDate = (new Date()).toDateString();
+                    break;
+                case TASK_FREQUENCY_TYPES[2].value:
+                    let today = new Date();
+                    if (today.getFullYear() == startingDate.getFullYear() &&
+                        today.getMonth() == startingDate.getMonth() &&
+                        today.getDate() == startingDate.getDate()) {
+                        nextDueDate = today.toDateString();
+                    }
+                    else {
+                        let _startingDate = new Date(startingDate);
+                        
+                    }
+                    break;
+                case TASK_FREQUENCY_TYPES[3].value:
+                    break;
+                case TASK_FREQUENCY_TYPES[4].value:
+                    break;
+            }
+        }
+    }
+
+    setCurrentTask(task: Task) {
         this.currentTask = task
     }
 
